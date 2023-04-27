fc28a57084d18362bef6d1f0e56c27463c782542
camilgg <cgarmendia@tracasa.es>
2023-04-27 12:15:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 from apps import home, heatmap, upload  # import your app modules here
 
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
-
+#test
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
 
